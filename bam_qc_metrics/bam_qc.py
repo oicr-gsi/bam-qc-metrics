fa991043efbf5c106c262cd3ec9003c95a576f29
iainrb <1695061+iainrb@users.noreply.github.com>
2019-05-24 17:50:51 -0400

    Read some simple samtools metrics using pysam

--- bam_qc_metrics/bam_qc.py
+++ bam_qc_metrics/bam_qc.py
@@ -2,7 +2,7 @@
 
 """Main script and class to compute BAM QC metrics"""
 
-import argparse, json, sys
+import argparse, csv, json, re, pysam, sys
 
 class bam_qc:
 
@@ -15,8 +15,27 @@ class bam_qc:
         "sample"
     ]
     
-    def __init__(self, metadata_path):
+    def __init__(self, bam_path, metadata_path, ref_path):
         with open(metadata_path) as f: self.metadata = json.loads(f.read())
+        self.bam_path = bam_path
+        self.ref_path = ref_path
+        self.samtools_metrics = self.run_samtools()
+        print(self.samtools_metrics)
+
+    def run_samtools(self):
+        result = pysam.stats(self.bam_path)
+        # parse output from 'samtools stats', filtering out comment lines
+        reader = csv.reader(
+            filter(lambda line: line!="" and line[0]!='#', re.split("\n", result)),
+            delimiter="\t"
+        )
+        # read some simple metrics
+        keys = ['RL', 'FRL', 'LRL']
+        metrics = {}
+        for row in reader:
+            if row[0] in keys:
+                metrics[row[0]] = [int(n) for n in row[1:]]
+        return metrics
         
     def write_output(self, out_path):
         output = {}
@@ -34,12 +53,16 @@ class bam_qc:
 
 def main():
     parser = argparse.ArgumentParser(description='QC for BAM files.')
+    parser.add_argument('-b', '--bam', metavar='PATH',
+                        help='Path to input BAM file')
     parser.add_argument('-m', '--metadata', metavar='PATH',
                         help='Path to JSON file containing metadata')
     parser.add_argument('-o', '--out', metavar='PATH',
                         help='Path for JSON output, or - for STDOUT')
+    parser.add_argument('-r', '--reference', metavar='PATH',
+                        help='Path to reference BED file')
     args = parser.parse_args()
-    qc = bam_qc(args.metadata)
+    qc = bam_qc(args.bam, args.metadata, args.reference)
     qc.write_output(args.out)
 
 if __name__ == "__main__":
